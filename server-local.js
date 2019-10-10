fa8058f8a9a8a121cade72f27653764f0172570b
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-10 21:46:34 +0100

    Added Netlify deployment setup

--- server-local.js
+++ server-local.js
@@ -2,4 +2,4 @@
 
 const app = require('./express/index.js');
 
-app.listen(4000, () => console.log('Local app listening on port 4000!'));
+app.listen(3000, () => console.log('Local app listening on port 3000!'));
