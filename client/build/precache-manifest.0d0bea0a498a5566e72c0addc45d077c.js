314e24f9713abfe48e4c1311dbaed65236275534
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-04 19:55:10 +0000

    Added build

---
+++ client/build/precache-manifest.0d0bea0a498a5566e72c0addc45d077c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a140a193850e4ee321267b6740a29af6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34f8600c990c66e9d9aa",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0806e94d15fb5339e2cf",
+    "url": "/static/js/2.afb0b0c9.chunk.js"
+  },
+  {
+    "revision": "34f8600c990c66e9d9aa",
+    "url": "/static/js/main.9bd12af6.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
