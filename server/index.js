07cfa978ceb3ca6d8092a28c48e752f7687e8c32
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-27 22:19:09 +0000

    Removed test endpoint

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const path = require('path');
 
-const userRouter = require('./users/userRouter');
+const userRouter = require('./routes/user');
 
 const server = express();
 
@@ -19,81 +19,6 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/users', userRouter);
-server.get('/api/test', (req, res) => {
-  res.json({
-    "id": "4y7vad8tv7as8v6s7a8",
-    "totalLocals": 1,
-    "locals": [
-      {
-        "number": 1,
-        "totalSensors": 1,
-        "name": "Local1",
-        "address": 11,
-        "sensors": [
-          {
-            "number": 1,
-            "name": "Sensor1deLocal1",
-            "Tipo_Sensor": {
-              "address": 13,
-              "value": 1,
-              "sign": "+/-"
-            },
-            "Dia_Min": {
-              "address": 13,
-              "value": 1,
-              "sign": "+/-"
-            },
-            "Dia_Max": {
-              "address": 13,
-              "value": 1,
-              "sign": "+/-"
-            },
-            "Noite_Max": {
-              "address": 13,
-              "value": 1,
-              "sign": "+/-"
-            },
-            "Hora_Dia_Hora": {
-              "address": 13,
-              "value": 1,
-              "sign": "+/-"
-            },
-            "Hora_Noite_Hora": {
-              "address": 13,
-              "value": 1,
-              "sign": "+/-"
-            },
-            "Hora_Dia_Minuto": {
-              "address": 13,
-              "value": 1,
-              "sign": "+/-"
-            },
-            "Hora_Noite_Minuto": {
-              "address": 13,
-              "value": 1,
-              "sign": "+/-"
-            },
-            "Porta do Controlador Sensor": {
-              "address": 13,
-              "value": 1,
-              "sign": "+/-"
-            },
-            "Porta do Controlador Rele": {
-              "address": 13,
-              "value": 1,
-              "sign": "+/-"
-            },
-            "Saida Normal Ou Invertida": {
-              "address": 13,
-              "value": 1,
-              "sign": "+/-"
-            },
-          }
-        ]
-      }
-    ]
-  })
-});
 
 server.get('/', (req, res) => {
   res.sendFile(appBuild);
